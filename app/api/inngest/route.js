86ea4b362497ad3cb3609a7d2b78e0039d368a6a
aman asati <asatiaman38@gmail.com>
2025-09-30 18:06:16 +0530

    First commit

---
+++ app/api/inngest/route.js
@@ -0,0 +1,21 @@
+import { serve } from "inngest/next";
+
+import { inngest } from "@/lib/inngest/client";
+import {
+  
+  checkBudgetAlerts,
+  processRecurringTransaction,
+  triggerRecurringTransactions,
+  generateMonthlyReports
+ 
+} from "@/lib/inngest/function";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+    checkBudgetAlerts,
+    triggerRecurringTransactions,
+    processRecurringTransaction,
+    generateMonthlyReports
+  ],
+});
